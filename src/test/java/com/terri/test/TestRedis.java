c49f57d0da3fffca6192aa7dc413e0dfca3890a8
terri <hongxuan0377@qq.com>
2016-09-23 14:21:38 +0800

    添加redis测试相关

--- src/test/java/com/terri/test/TestRedis.java
+++ src/test/java/com/terri/test/TestRedis.java
@@ -1,8 +1,14 @@
 package com.terri.test;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +16,9 @@ import org.junit.Test;
 import com.terri.sys.util.RedisUtil;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Tuple;
+import sun.security.tools.jarsigner.TimestampedSigner;
 
 public class TestRedis {
     private Jedis jedis; 
@@ -18,11 +27,219 @@ public class TestRedis {
     public void setup() {
         //连接redis服务器，192.168.0.100:6379
    //     jedis = new Jedis("192.168.0.100", 6379);
-    	   jedis = new Jedis("localhost", 6379);
+    //	   jedis = new Jedis("localhost", 6379);
         //权限认证
    //     jedis.auth("admin");  
+    // 	jedis = new Jedis("gz.redis.cache.chinacloudapi.cn", 6379);
+    //  jedis.auth("pP/BXXELrGVF3kGWvMWFYbVdfOTGMKUdOFSaV2Tf1k8=");
+    	
+   // 	 jedis = new Jedis("redis-statis.chinacloudapp.cn", 6379);
+   //	 jedis.auth("M2VjMGIxZGM0M2NjOGZjOGM2ZjkyNTAw");
+      
+    	     
+    }
+    @Test
+    public void  RemoveAfterkeys(){
+    	GregorianCalendar gc =new GregorianCalendar();
+    	SimpleDateFormat sf  =new SimpleDateFormat("yyyyMMddHH");
+    	    Date d=Calendar.getInstance().getTime();
+    	    gc.setTime(d);
+    	 //   gc.add(GregorianCalendar.HOUR,1);
+    	    for(int i=1;i<60;i++){
+    	    	  System.out.println("del "+sf.format(gc.getTime()).substring(0,8)); 
+    	    	  String key3="wifilog:b:v1:"+sf.format(gc.getTime()).substring(0,8);
+    	    	  String key4="wifilog:b:v2:"+sf.format(gc.getTime()).substring(0,8);
+    	    	  String key5="wifilog:t:v1:"+sf.format(gc.getTime()).substring(0,8);
+    	    	  String key6="wifilog:t:v2:"+sf.format(gc.getTime()).substring(0,8);
+    	    	   for(int j=0;j<24;j++){
+    	    	       gc.add(GregorianCalendar.HOUR,1);
+    	    	       String key1="wifilog:b:wlan:user:"+sf.format(gc.getTime());
+    	    	       String key2="wifilog:t:wlan:user:"+sf.format(gc.getTime());
+    	    	       jedis.del(key1,key2); 
+              System.out.println("del "+key1+" "+ key2); 	  
+    	    	            }
+    	    	   jedis.del(key3,key4,key5,key6); 
+    	         }
+
     }
     
+    @Test
+    public void  RemoveBeforekeys(){
+    	GregorianCalendar gc =new GregorianCalendar();
+    	SimpleDateFormat sf  =new SimpleDateFormat("yyyyMMddHH");
+    	    Date d=Calendar.getInstance().getTime();
+    	    gc.setTime(d);
+    	            
+    	    gc.add(GregorianCalendar.DAY_OF_YEAR,-7);
+    	    gc.set(GregorianCalendar.HOUR_OF_DAY,0);
+    	    for(int i=0;i<300;i++){
+    	    	  long start = System.currentTimeMillis();
+    	    	  Pipeline p = jedis.pipelined();
+    	    	   String key_t="wifilog:t:v1:"+sf.format(gc.getTime()).substring(0,8);
+    	    	  // gc.add(GregorianCalendar.DAY_OF_MONTH,-1);
+       	    	System.out.println("del "+key_t); 	 
+    	    	      //jedis.del(key_t);
+       	    	 p.del(key_t);
+       	     String key_t2="wifilog:t:v2:"+sf.format(gc.getTime()).substring(0,8);
+ 	    	         System.out.println("del "+key_t2); 	 
+ 	    	 	      p.del(key_t2);
+ 	    	         
+    	       	 String key_b="wifilog:b:v1:"+sf.format(gc.getTime()).substring(0,8);
+       	    	 System.out.println("del "+key_b); 	 
+    	    	    p.del(key_b); 
+       	    	 String key_b2="wifilog:b:v2:"+sf.format(gc.getTime()).substring(0,8);
+       	    	 System.out.println("del "+key_b2); 
+       	    	 
+    	    	    p.del(key_b2); 
+ 
+    	    	   String kpvut="wifilog:t:wlan:pv:url:"+sf.format(gc.getTime()).substring(0,8);
+    	    	   System.out.println("del "+kpvut); 
+    	    	   p.del(kpvut); 
+    	    	   
+    	       	   String kpvub="wifilog:b:wlan:pv:url:"+sf.format(gc.getTime()).substring(0,8);
+    	    	   System.out.println("del "+kpvub); 
+    	    	   p.del(kpvub); 
+    	    	   
+    	    	   
+    	    	   for(int j=0;j<24;j++){
+    	    	       gc.add(GregorianCalendar.HOUR,-1);
+    	    	       String keyh="wifilog:b:wlan:user:"+sf.format(gc.getTime());
+    	    	       System.out.println("del "+keyh); 	  
+    	    	       p.del(keyh); 
+        
+                String keyht="wifilog:t:wlan:user:"+sf.format(gc.getTime());
+                System.out.println("del "+keyht);
+       	   	       p.del(keyht); 
+             	
+                String keyhbl="wifilog:b:lan:user:"+sf.format(gc.getTime());
+   	           System.out.println("del "+keyhbl); 	  
+        	       p.del(keyhbl); 
+
+               String keyhtl="wifilog:t:lan:user:"+sf.format(gc.getTime());
+               System.out.println("del "+keyhtl); 	
+	      	       p.del(keyhtl); 
+           
+    	    	            }
+    	    	   List<Object> results = p.syncAndReturnAll();
+    	    	   long end = System.currentTimeMillis();
+    	    	    System.out.println(gc.getTime()+" Pipelined del: " + ((end - start)/1000.0) + " seconds");
+    	    	//    jedis.disconnect();
+    	         }
+    }
+        @Test
+        public void getUrlPv(){
+        	
+        	GregorianCalendar gc =new GregorianCalendar();
+        	SimpleDateFormat sf  =new SimpleDateFormat("yyyyMMdd");
+         Date d=Calendar.getInstance().getTime();
+         gc.setTime(d);
+        	gc.add(GregorianCalendar.DAY_OF_YEAR,-30);
+         StringBuilder sb1=new StringBuilder();
+         StringBuilder sb2=new StringBuilder();
+        	gc.add(gc.HOUR,-15);
+        	Character filedS=',';
+        	for(int i=0;i<31;i++){
+        		 // System.out.println("火车:"+sf.format(gc.getTime()));
+        		  sb1.append("火车:"+sf.format(gc.getTime())).append(System.lineSeparator());
+        		  String key1="wifilog:t:wlan:pv:url:"+sf.format(gc.getTime());
+        		  Set<Tuple> s=jedis.zrevrangeWithScores(key1, 0, 50);
+        		  for(Tuple tmp:s){
+	  	    	     //  System.out.println(tmp.getElement()+filedS+Math.round(tmp.getScore()));
+	  	    	       sb1.append(tmp.getElement()+filedS+Math.round(tmp.getScore())).append(System.lineSeparator());
+	  	                       }
+        		//  System.out.println("大巴"+sf.format(gc.getTime()));
+        		  sb2.append("大巴:"+sf.format(gc.getTime())).append(System.lineSeparator());
+        		  Set<Tuple> s2=jedis.zrevrangeWithScores(key1, 0, 50);
+        		  for(Tuple tmp:s2){
+	  	    	   //    System.out.println(tmp.getElement()+filedS+Math.round(tmp.getScore()));
+	  	    	       sb2.append(tmp.getElement()+filedS+Math.round(tmp.getScore())).append(System.lineSeparator());
+	  	                       }
+        		  gc.add(gc.DAY_OF_YEAR,1);     
+        	        }
+        System.out.println(sb1.toString()+sb2.toString());
+        	
+        }
+     @Test 
+     public void getRange(){
+    	 java.text.NumberFormat nf = java.text.NumberFormat.getInstance();   
+    	 nf.setGroupingUsed(false);  
+
+     	GregorianCalendar gc =new GregorianCalendar();
+     	SimpleDateFormat sf  =new SimpleDateFormat("yyyyMMddHH");
+ 
+	    Date d=Calendar.getInstance().getTime();
+	    gc.setTime(d);
+	    gc.add(gc.HOUR,-13);
+	    //gc.add(gc.DAY_OF_YEAR,-1);	
+	    for(int k=0;k<7;k++){
+	
+				 double inScore=0.0;
+				 double outScore=0.0;
+			    	Set<String> inS=new HashSet<String>();	 
+			    Set<String> outS=new HashSet<String>();	 
+			    Set<String> s=new HashSet<String>();	 
+
+				    
+		    for(int i=0;i<24;i++){	
+		    	 gc.add(gc.HOUR,-1);
+		    	 //System.out.println(sf.format(gc.getTime()));
+		  	  String key_t="wifilog:t:wlan:user:"+sf.format(gc.getTime()).substring(0,10);
+		  	  Set<Tuple> t1=jedis.zrangeWithScores(key_t, 0, -1);
+		  	    for(Tuple tmp:t1){
+		  	    	      outScore+=tmp.getScore();
+		  	    	      outS.add(tmp.getElement());
+		  	    	      s.add(tmp.getElement());
+		  	          }
+		  	 	key_t="wifilog:t:lan:user:"+sf.format(gc.getTime()).substring(0,10);
+		    Set<Tuple> t2=jedis.zrangeWithScores(key_t, 0, -1);
+	  	    for(Tuple tmp:t2){
+	  	    	      inScore+=tmp.getScore();
+	  	    	       inS.add(tmp.getElement());
+	  	    	     s.add(tmp.getElement());
+	  	        }
+	  	            
+	  	           }
+	  	    System.out.println(sf.format(gc.getTime()).substring(0,8)+"　外网用户数"+outS.size()+" pv数："+nf.format(inScore)+" 内网用户数："+inS.size()+" pv数"+nf.format(outScore) +"总uv数："+s.size());
+	    }
+
+     }
+    @Test
+    public void statistics(){
+    	GregorianCalendar gc =new GregorianCalendar();
+    	SimpleDateFormat sf  =new SimpleDateFormat("yyyyMMddHH");
+    	Set<String> s=new HashSet<String>();
+    	   
+    	    Date d=Calendar.getInstance().getTime();
+    	    gc.setTime(d);
+    	    gc.add(gc.DAY_OF_YEAR,-3);
+    	    gc.add(gc.HOUR,-16);
+    	            Double sc=0.0;
+    	        for(int i=0;i<24;i++){	        	  
+    	        	  String key_t="wifilog:b:wlan:user:"+sf.format(gc.getTime()).substring(0,10);
+    	        	  System.out.println(key_t);
+    	        	 Set<String> t=jedis.zrange(key_t, 0,-1);
+    	        	 
+    	        	  System.out.println("size="+t.size());
+    	        	  s.addAll(t);
+    	        	  gc.add(gc.HOUR,1);
+    	        	     // for(String tmp:t){
+    	        	    	 //   sc+=jedis.zscore(key_t,tmp); 
+    	        	                  //}
+    	        	                  
+    	        	  	
+    	                  }
+    	        System.out.println("size:"+s.size()+" score:"+sc);
+    	        
+    	
+    }
+    @Test
+    public void RemoveYearKeys(){
+    	    String year="2015";
+    	    Set<String> keys=jedis.keys("wifilog:b:wlan:user:"+year+".*");
+    	    for(String key:keys){
+    	    	  System.out.println("del "+key); 	 
+    	    }
+    }
     /**
      * redis存储字符串
      */
